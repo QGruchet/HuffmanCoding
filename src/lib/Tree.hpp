182a91b5643fb0c874e93247208a51716c11fa32
m27bay <mickael.ledenmat@gmail.com>
2020-11-09 22:34:28 +0100

    Merge

--- src/lib/Tree.hpp
+++ src/lib/Tree.hpp
@@ -25,23 +25,12 @@ class Sommet
         
         int maximum(int a, int b);
 
-<<<<<<< HEAD
-    /* Getters */
-    int getNumCar() const;
-    char getCar() const;
-    Sommet* getLeft() const;
-    Sommet* getRight() const;
-    int getDepth(Sommet*);
-=======
         /* Getters */
         int getNumCar() const;
         char getCar() const;
         Sommet* getLeft() const;
         Sommet* getRight() const;
         int getDepth(Sommet* sommet);
->>>>>>> 9989db358e97b8c4612d38b6fbad8187acb968a0
-
-
 
         /* Destructor */
         ~Sommet();
@@ -65,18 +54,6 @@ class ArbreB
         ArbreB &operator+=(const ArbreB&); // Add an other tree ( Quentin )
         ArbreB &operator-=(const ArbreB&); // Split tree ( Quentin )
 
-<<<<<<< HEAD
-    void operator<(Sommet&); // Add node
-    int operator>(Sommet&); // Dell node ( Quentin )
-
-    int emptyTree();
-    ArbreB addNode(Sommet *);
-
-    Sommet &operator[](int index);
-
-    /*printer*/
-    void printTree(Sommet*, int depth);
-=======
         void operator<(Sommet newRoot); // Add node
         void operator>(Sommet* newRoot); // Dell node ( Quentin )
 
@@ -84,7 +61,6 @@ class ArbreB
 
         /*printer*/
         void printTree(ArbreB, int depth);
->>>>>>> 9989db358e97b8c4612d38b6fbad8187acb968a0
 
         /* Getters */
         Sommet* getRoot() const;
