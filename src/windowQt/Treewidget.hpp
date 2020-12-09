006c33917d044b57f25268391bffa13cbd750da8
m27bay <mickael.ledenmat@gmail.com>
2020-12-09 17:12:03 +0100

    Update comments

--- src/windowQt/Treewidget.hpp
+++ src/windowQt/Treewidget.hpp
@@ -1,5 +1,5 @@
-#ifndef TREEWIDGET_H
-#define TREEWIDGET_H
+#ifndef TREEWIDGET_HPP
+#define TREEWIDGET_HPP
 
 #include <QWidget>
 #include "../lib/Tree.hpp"
@@ -8,15 +8,23 @@ class TreeWidget : public QWidget
 {
     Q_OBJECT
 private:
+    /* Data */
     ArbreB tree;
 
 public:
+    /* Constructor */
     explicit TreeWidget(QWidget* parent = nullptr);
+
+    /* Functions */
     void setTree(ArbreB);
     void draw(QPainter*, int, int);
 
+    /* Destructor */
+    ~TreeWidget();
+
 protected:
+    /* paint */ 
     void paintEvent(QPaintEvent*);
 };
 
-#endif // TREEWIDGET2_H
+#endif // TREEWIDGET_HPP
