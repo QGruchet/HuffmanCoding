32d7141eb90a6a39b4771dc929c2381c06d93249
m27bay <mickael.ledenmat@gmail.com>
2020-12-10 16:09:40 +0100

    Add comments and doc

--- src/windowQt/Mytextedit.cpp
+++ src/windowQt/Mytextedit.cpp
@@ -19,33 +19,55 @@ MyTextEdit::MyTextEdit(QWidget *parent)
     setPalette(pal);
 }
 
+/**
+ * *Description : clear default text.
+ * @param event, not used.
+ * */
 void MyTextEdit::mousePressEvent(QMouseEvent *event) {
     Q_UNUSED(event); // unused param.
 
     if(clicDellText) {
-        clear(); // Clear information text.
+        clear();
         clicDellText = false;
     }
 }
 
+/**
+ * *Description : set a new text info.
+ * @param information, new text info.
+ * */
 void MyTextEdit::setInfo(QString information) {
     textInfo = information;
 }
 
+/**
+ * *Description : get text info.
+ * @return info.
+ * */
 QString MyTextEdit::info() const {
     return textInfo;
 }
 
+/**
+ * *Description : set boolean if clic on textEdit dell the text.
+ * @param newClicDellText, the new boolean.
+ * */
 void MyTextEdit::setClicDellText(bool newClicDellText) {
     clicDellText = newClicDellText;
 }
 
+/**
+ * *Description : write text info in the textEdit.
+ * */
 void MyTextEdit::writeInfo() {
     setTextColor(Qt::darkGray);
     setText(textInfo);
     setTextColor(Qt::black);
 }
 
+/**
+ * *Description : Destructor.
+ * */
 MyTextEdit::~MyTextEdit()
 {
     // Empty
